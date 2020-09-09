34b79a75defaf82ef63374bdece6d13e347f1991
Bene Arinci <benedetta.arinci@gmail.com>
2020-09-09 12:09:03 +0100

    Generated project json file

---
+++ src/projects.js
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    title: "Bank Tech Task",
+    description: "Small client-side app developed using <b>JavaScript</b>, <b>Jasmine</b>, <b>ESLint</b>. It allows a user to create a bank account, to make deposits and withdrawals and to be able to print their bank statement in a specific format"
+  },
+  {
+    title: "Bowling ScoreCard",
+    description: "Single page interactive web app developed using <b>JavaScript</b>, <b>JQuery</b>, <b>Jasmine</b>, <b>HTML</b>, <b>CSS</b>, <b>Bootstrap</b>. It is easy to use. It allows the user to enter the points for each frame and it is constantly updating the score"
+  },
+  {
+    title: "CodeQuest()",
+    description: "platform game for children with the aim of helping them to develop problem solving skills and, possibly, bringing them closer to programming. This was my final group project, presented during the Makers’ Demo Day on July 31 2020. The app was developed using the following tech stack: JavaScript, React, Node.js, HTML5, CSS, Tachyons, Jest"
+  },
+  {
+    title: "Acebook",
+    description: "group project I worked on during the bootcamp’s week 8 and 9. The aim was to create an app similar to FaceBook. Tech stack: Ruby on Rails, RSpec, Capybara, PostgreSQL, HTML5, CSS, Bootstrap, Rubocop, Travis"
+  }
+];
