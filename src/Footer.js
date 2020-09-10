b9d3e5bd190413a779c5a3350a53206fc1eced10
Bene Arinci <benedetta.arinci@gmail.com>
2020-09-10 18:16:16 +0100

    Added info to footer. Played with CSS

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,19 @@ import './Footer.css'
 
 function Footer () {
   return (
-    <div className = "footer">
-      test
+    <div className = "footer flex justify-around">
+      <div style= {{display:"flex", justifyContent:"flex-end", width:"800px", marginLeft:"auto"}}>
+      <p className="phone">+44(0)7510060718</p>
+      <p className="email">benedetta.arinci@gmail.com</p>
+      <a href="https://github.com/BeneArinci">
+       <img alt='git' src="./assets/images/github.png" className="githubIcon" />
+      </a>
+      <a href="https://www.linkedin.com/in/benedetta-arinci/" width="300px">
+       <img alt='linkedin' src="./assets/images/linkedin.png" className="linkedinIcon" />
+      </a>
+      </div>
+      
+    
     </div>
   )
 }
