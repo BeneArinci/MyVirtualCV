94a6f8b8dad647d7e3ce992eb44ea2ace721a489
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-12 00:20:05 +0100

    Responsive Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,14 @@ import './Footer.css'
 
 function Footer () {
   return (
-    <div className = "footer flex justify-around">
-      <div style= {{display:"flex", justifyContent:"flex-end", width:"800px", marginLeft:"auto"}}>
-      <p className="phone">+44(0)7510060718</p>
-      <p className="email">benedetta.arinci@gmail.com</p>
+    <div className = "footer">
+      <div style= {{display:"flex", justifyContent:"flex-end", width:"100%", marginLeft:"auto"}}>
+      <p className="phone f7 f4-ns f4-m f4-l">+44(0)7510060718</p>
+      <p className="email f7 f4-ns f4-m f4-l">benedetta.arinci@gmail.com</p>
       <a href="https://github.com/BeneArinci">
        <img alt='git' src="./assets/images/github.png" className="githubIcon" />
       </a>
-      <a href="https://www.linkedin.com/in/benedetta-arinci/" width="300px">
+      <a href="https://www.linkedin.com/in/benedetta-arinci/" >
        <img alt='linkedin' src="./assets/images/linkedin.png" className="linkedinIcon" />
       </a>
       </div>
