1490bfc6df6c79d103cfcdc3e6f747ee36447d0e
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-11 12:42:10 +0100

    Optimized Scroll styling

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Scroll (props) {
   //props children refers to anything is wrapped into a non self closing component. In our case is the cardlist
   return(
-    <div className='tc' style = {{overflow: 'scroll', border: '1px solid lightGreen', height: '580px', width: '50%', margin:'auto'}}>
+    <div className='tc' style = {{overflow: 'scroll', border: '1px solid lightGreen', width: '50%', margin:'auto'}}>
       {props.children}
     </div>
   );
