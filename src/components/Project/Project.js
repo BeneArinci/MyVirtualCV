01705b66c2cf9b6cd155e93584af59a494d5f571
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-11 12:11:16 +0100

    Project component now responsive

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 function Project ({imageURL, title, description, githubpage}) {
   return (
-<article className="center mw5 mw6-ns br3 hidden mv0" style={{margin:"auto", marginBottom:"30px"}}>
-  <img alt="bank" src={imageURL} className="db w-100 br2 br--top" width="600" height="300"/>
+<article className="center mw5 mw6-ns br3 hidden mv0" style={{margin:"auto", marginBottom:"3rem"}}>
+  <img alt="project" src={imageURL} className="db w-100 br2 br--top" style={{height: "auto"}}/>
   <div className="pa2 ph3-ns pb3-ns center">
     <div className="dt w-100 mt1">
       <div className="dtc">
-        <h1 className="dark-green f5 pa3 f3-ns mv0">{title}</h1>
+        <h1 className="dark-green f5 pa3 f3-ns mv0 w-100">{title}</h1>
       </div>
     </div>
-    <div className="f3 f5-ns lh-copy measure">
-    <p className="f3 f5-ns lh-copy measure">
+    <div className="f5-ns lh-copy measure w-100">
+    <p className="f5-ns w-100 lh-copy measure">
     {description} <br></br>
-    For more info visit: <a href = {githubpage}>{githubpage}</a>
+    For more info visit: <a className="flex justify-center w-100 lh-copy measure" href = {githubpage}>{githubpage}</a>
     </p > 
      
 
