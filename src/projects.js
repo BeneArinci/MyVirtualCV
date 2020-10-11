35d51146120c43b44477569a89d934ad3a99de2b
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-10-11 11:25:34 +0100

    Added party-planning project to my list

--- src/projects.js
+++ src/projects.js
@@ -1,34 +1,41 @@
 export const projects = [
   {
     id: 1,
-    imageURL: "./assets/images/BankTechTest.png",
-    title: "Bank Tech Task",
-    description: "Small client-side app developed using JavaScript, Jasmine, ESLint. It allows a user to create a bank account, to make deposits and withdrawals and to be able to print their bank statement in a specific format.",
-    githubpage: "https://github.com/BeneArinci/Bank_Tech_Test"
+    imageURL: "./assets/images/party-planning.png",
+    title: "Party Planning Project",
+    description: "This project was a tech task I was recently asked to work on (end of September 2020). I was asked to give the possibility to a not-signed-in user to get access to a list of suppliers and add one or more extra functionalities. I developed this web-app using React and, more specifically, the library create-react-app. I also had the opportunity to put into practice my newly built React testing skills using Jest and to deal with multiple API calls.",
+    githubpage: "https://github.com/BeneArinci/Party_Planning_Project"
   },
   {
-    id: 2,
+    id: 3,
     imageURL: "./assets/images/bowling.png",
     title: "Bowling ScoreCard",
     description: "Single page interactive web app developed using JavaScript, JQuery, Jasmine, HTML, CSS, Bootstrap. It is easy to use. It allows the user to enter the points for each frame and it is constantly updating the score.",
     githubpage: "https://github.com/BeneArinci/Bowling-ScoreCard"
   },
   {
-    id: 3,
+    id: 4,
     imageURL: "./assets/images/portfolio.png",
     title: "MyPortfolio",
     description: "Yes, this is the web-app you are looking at :) I developed it using React, JavaScript, JSX",
     githubpage: "https://github.com/BeneArinci/MyVirtualCV"
   },
   {
-    id: 4,
+    id: 5,
     imageURL: "./assets/images/codequest.png",
     title: "CodeQuest()",
     description: "Platform game for children with the aim of helping them to develop problem solving skills and, possibly, bringing them closer to programming. This was my final group project, presented during the Makers’ Demo Day on July 31 2020. The app was developed using the following tech stack: JavaScript, React, Node.js, HTML5, CSS, Tachyons, Jest.",
     githubpage: "https://github.com/BeneArinci/CodeQuest"
   },
   {
-    id: 5,
+    id: 6,
+    imageURL: "./assets/images/BankTechTest.png",
+    title: "Bank Tech Task",
+    description: "Small client-side app developed using JavaScript, Jasmine, ESLint. It allows a user to create a bank account, to make deposits and withdrawals and to be able to print their bank statement in a specific format.",
+    githubpage: "https://github.com/BeneArinci/Bank_Tech_Test"
+  },
+  {
+    id: 7,
     imageURL: "./assets/images/acebook.png",
     title: "Acebook",
     description: "Group project I worked on during the bootcamp’s weeks 8 and 9. The aim was to create an app similar to FaceBook. Tech stack: Ruby on Rails, RSpec, Capybara, PostgreSQL, HTML5, CSS, Bootstrap, Rubocop, Travis.",
